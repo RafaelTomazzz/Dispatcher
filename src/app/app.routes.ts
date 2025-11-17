38f9d25b3ad814b57c14065b910a1e49f03db731
RAFAEL TOMAZ DE OLIVEIRA AQUINO <100018@pmc.local>
2025-11-17 15:51:37 -0300

    FirstCommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login-component/login-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LoginComponent
+    }
+];
