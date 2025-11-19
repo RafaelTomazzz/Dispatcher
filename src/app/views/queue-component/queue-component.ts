f25cbe3a42f54c639816334c4626b6addb23299f
JEAN CARLOS DE SOUZA MOURA <100229@pmc.local>
2025-11-19 15:41:02 -0300

    queue-page-remodel

--- src/app/views/queue-component/queue-component.ts
+++ src/app/views/queue-component/queue-component.ts
@@ -48,14 +48,14 @@ export class QueueComponent implements OnInit {
   ticket3: Ticket = {
     id: 19700,
     name: "Ponto de rede não funciona",
-    requesttype_id: " SEDUC Sede",
+    requesttype_id: " SEDUC - Sede Indaiá",
     date_creation: new Date(2025, 10, 10),
     urgency: UrgencyTypeEnum.Baixa
   }
 
   ticket4: Ticket = {
     id: 19851,
-    name: "A internet do prédio não funcionar",
+    name: "A internet do prédio não funciona",
     requesttype_id: "UBS Porto Novo",
     date_creation: new Date(2025, 10, 19),
     urgency: UrgencyTypeEnum.MuitoAlta
@@ -63,8 +63,8 @@ export class QueueComponent implements OnInit {
 
   ticket5: Ticket = {
     id: 19698,
-    name: "Micro de laboratória com lentidão",
-    requesttype_id: "Emef Uijo",
+    name: "Micro de laboratório com lentidão",
+    requesttype_id: "EMEF Maria Ujio",
     date_creation: new Date(2025, 10, 10),
     urgency: UrgencyTypeEnum.MuitoBaixa
   }
