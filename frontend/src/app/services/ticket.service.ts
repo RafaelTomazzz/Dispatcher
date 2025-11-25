e9ba0a9cf0a8dcc444f2e6f45dbd4a5776aa8a53
RAFAEL TOMAZ DE OLIVEIRA AQUINO <100018@pmc.local>
2025-11-25 15:15:48 -0300

    entities_id_text_cut

--- frontend/src/app/services/ticket.service.ts
+++ frontend/src/app/services/ticket.service.ts
@@ -9,14 +9,18 @@ import { map } from 'rxjs';
 })
 export class TicketService {
   private http = inject(HttpClient)
-  private apiUrl = "http://192.168.11.235:8000/api/queue"
+  private apiUrl = "http://localhost:8000/api/queue"
 
   getListQueue(): Observable<Ticket[]>{
     return this.http.get<any>(`${this.apiUrl}/fetchqueue?saved_search_id=164`).pipe(
+      // map((ticketResponse) => {
+      //   ticketResponse.queue_tickets.map()
+      // })
+      
       map((ticketResponse) => {
         return ticketResponse.queue_tickets.map((ticket:any) => ({
             id: ticket.id,
-            entities_id: ticket.entities_id,
+            entities_id: ticket.entities_id.split("PMC > ", 2).slice(1),
             name: ticket.name,
             urgency: ticket.urgency,
             locations_id: ticket.locations_id,
